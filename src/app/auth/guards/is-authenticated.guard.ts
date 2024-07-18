dbbed9a7b2ee2a6443107706cc6c0ef7c27cffd9
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-18 17:12:31 -0600

    feat: logic of new guard

--- src/app/auth/guards/is-authenticated.guard.ts
+++ src/app/auth/guards/is-authenticated.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { AuthStatus } from '../interfaces';
 
 export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
-  return true;
+
+  const authService = inject(AuthService);
+  const router      = inject(Router);
+
+  if(authService.authStatus()=== AuthStatus.authenticated) return true;
+
+  router.navigateByUrl('/auth/login')
+  return false;
 };
