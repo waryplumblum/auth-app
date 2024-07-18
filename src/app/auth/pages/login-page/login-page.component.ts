7ea6c4af9f9e2c83f1141d0eb6430bcd0426acca
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-18 12:30:12 -0600

    feat: login form

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  public myForm:FormGroup = this.fb.group({
+    email: ['', [ Validators.required, Validators.email ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+  });
+
+  login(){
+    console.log(this.myForm.value);
+
+  }
 
 }
