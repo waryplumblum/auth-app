146dfc41dad84fded8c8364064181e0cd28f323b
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-18 17:29:26 -0600

    feat: optimization code setAuthentication

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -21,6 +21,13 @@ export class AuthService {
 
   constructor() { }
 
+  private setAuthentication(user:User,token:string):boolean{
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+    localStorage.setItem('token',token);
+    return true;
+  }
+
   login(email:string,password:string):Observable<boolean>{
 
     const url  = `${ this.baseUrl }/auth/login`;
@@ -28,15 +35,7 @@ export class AuthService {
 
     return this.http.post<LoginResponse>(url,body)
       .pipe(
-        tap( ({ user, token }) => {
-          this._currentUser.set(user);
-          this._authStatus.set(AuthStatus.authenticated);
-          localStorage.setItem('token',token);
-
-
-        }),
-        map( () => true ),
-        //TODO: errores
+        map( ({user,token}) => this.setAuthentication(user,token)),
         catchError( err => throwError( () => err.error.message)
         )
       );
@@ -52,12 +51,7 @@ export class AuthService {
 
       return this.http.get<CheckTokenResponse>(url,{headers})
       .pipe(
-        map(({token,user}) => {
-          this._currentUser.set(user);
-          this._authStatus.set(AuthStatus.authenticated);
-          localStorage.setItem('token',token);
-          return true;
-        }),
+        map( ({user,token}) => this.setAuthentication(user,token)),
         catchError(()=>{
           this._authStatus.set(AuthStatus.notAuthenticated)
           return of(false);
