8ea2ed35a77c54964d797d89eb8903cc216cf3cc
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-18 18:29:24 -0600

    feat: isNotAuthenticated guard

--- src/app/auth/guards/is-not-authenticated.guard.ts
+++ src/app/auth/guards/is-not-authenticated.guard.ts
@@ -3,14 +3,15 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { AuthStatus } from '../interfaces';
 
-export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
+export const isNotAuthenticatedGuard: CanActivateFn = (route, state) => {
 
   const authService = inject(AuthService);
   const router      = inject(Router);
 
-  if(authService.authStatus()=== AuthStatus.authenticated) return true ;
-  // if(authService.authStatus()=== AuthStatus.checking     ) return false;
+  if(authService.authStatus()=== AuthStatus.authenticated) {
+    router.navigateByUrl('/dashboard')
+    return false;
+  }
 
-  router.navigateByUrl('/auth/login')
-  return false;
+  return true;
 };
